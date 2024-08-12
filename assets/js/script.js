3ad69ac2c58bb28d529f23688673072af0011733
Jack Hunt <kegz4uv6@students.codeinstitute.net>
2024-08-12 10:59:36 +0000

    Add event listeners for document and loading button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
-function rungame() {
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
@@ -14,18 +29,18 @@ function incrementScore() {
     
 }
 
-function incrementWrongAnswers() {
+function incrementWrongAnswer() {
     
 }
 
-function displayAddisionQuestion() {
+function displayAdditionQuestion() {
     
 }
 
-function displaySubtractQuestio() {
+function displaySubtractQuestion() {
     
 }
 
-function displayMultiplyQuestio() {
+function displayMultiplyQuestion() {
     
 }
