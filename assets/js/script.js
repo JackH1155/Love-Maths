0d30adeed0602ba194dc0fc3e79024fed2f5ab0a
Jack Hunt <kegz4uv6@students.codeinstitute.net>
2024-08-12 10:38:47 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+    
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswers() {
+    
+}
+
+function displayAddisionQuestion() {
+    
+}
+
+function displaySubtractQuestio() {
+    
+}
+
+function displayMultiplyQuestio() {
+    
+}
